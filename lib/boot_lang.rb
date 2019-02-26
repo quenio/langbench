4d67bedf3f250e3615540329b61299c9911b4c59
quenio <queniodossantos@gmail.com>
2019-02-26 14:04:54 -0300

    Moved a generic tokenizer, parser and renderer to LangLang.

--- lib/boot_lang.rb
+++ lib/boot_lang.rb
@@ -5,11 +5,17 @@ module BootLang
 
   include LangLang
 
-  def self.translate(source)
-    translator = Translator.new tokenizer: Tokenizer.new,
-                                parser: Parser.new,
-                                renderer: Renderer.new
-    translator.translate source
+  REGEX = %r{\A\s*<([A-Za-z0-9]+)>\s*</([A-Za-z0-9]+)>\s*\z}.freeze
+
+  RULE = lambda do |tokens|
+    raise "Expected 2 tokens but found: #{tokens}" if tokens.length != 2
+    raise "Expected matching elements but found: #{tokens}" if tokens[0] != tokens[1]
+
+    Element.new(tokens[0])
+  end
+
+  TEMPLATE = lambda do |element|
+    "<div class=\"#{element.name}\"></div>"
   end
 
   class Element
@@ -19,33 +25,6 @@ module BootLang
     end
 
     attr_reader :name
-
-  end
-
-  class Tokenizer
-
-    def tokenize(source)
-      source.scan(%r{\A\s*<([A-Za-z0-9]+)>\s*</([A-Za-z0-9]+)>\s*\z})[0]
-    end
-
-  end
-
-  class Parser
-
-    def parse(tokens)
-      raise "Expected 2 tokens but found: #{tokens}" if tokens.length != 2
-      raise "Expected matching elements but found: #{tokens}" if tokens[0] != tokens[1]
-
-      Element.new(tokens[0])
-    end
-
-  end
-
-  class Renderer
-
-    def render(element)
-      "<div class=\"#{element.name}\"></div>"
-    end
 
   end
 
--- lib/lang_lang.rb
+++ lib/lang_lang.rb
@@ -2,6 +2,17 @@
 
 module LangLang
 
+  def self.included(base)
+    base.class_eval do
+      def self.translate(source)
+        translator = Translator.new tokenizer: Tokenizer.new(regex: self::REGEX),
+                                    parser: Parser.new(rule: self::RULE),
+                                    renderer: Renderer.new(template: self::TEMPLATE)
+        translator.translate source
+      end
+    end
+  end
+
   class Translator
 
     attr_reader :tokenizer
@@ -16,6 +27,48 @@ module LangLang
 
     def translate(source)
       @renderer.render(@parser.parse(@tokenizer.tokenize(source)))
+    end
+
+  end
+
+  class Tokenizer
+
+    attr_reader :regex
+
+    def initialize(options = {})
+      @regex = options[:regex]
+    end
+
+    def tokenize(source)
+      source.scan(@regex)[0]
+    end
+
+  end
+
+  class Parser
+
+    attr_reader :rule
+
+    def initialize(options = {})
+      @rule = options[:rule]
+    end
+
+    def parse(tokens)
+      @rule.call(tokens)
+    end
+
+  end
+
+  class Renderer
+
+    attr_reader :template
+
+    def initialize(options = {})
+      @template = options[:template]
+    end
+
+    def render(element)
+      @template.call(element)
     end
 
   end
