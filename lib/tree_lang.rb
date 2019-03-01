dac4c9fc72bbe1c70d9dd7630908ae1158b5b939
quenio <queniodossantos@gmail.com>
2019-03-01 18:33:48 -0300

    Extracted PrettyPrinter in its own file.

--- lib/tree_lang.rb
+++ lib/tree_lang.rb
@@ -1,4 +1,4 @@
-# struct_lang.rb
+require 'pretty_printer'
 
 module TreeLang
 
@@ -37,44 +37,6 @@ module TreeLang
 
     def emit_content(_value)
       raise 'Not implemented.'
-    end
-
-  end
-
-  module PrettyPrinter
-
-    def init_indentation
-      @indent = ''
-      @indent_next = true
-      @line_changed = [false]
-    end
-
-    def indent_print(value)
-      print @indent_next ? "\n#{@indent}#{value}" : value
-    end
-
-    def indent
-      @indent += '  '
-    end
-
-    def unindent
-      @indent.chomp!('  ')
-    end
-
-    def inline
-      @indent_next = false
-    end
-
-    def enter_section
-      @line_changed.push(@indent_next)
-    end
-
-    def exit_section
-      if @indent_next
-        @line_changed.pop
-      else
-        @indent_next = @line_changed.pop
-      end
     end
 
   end
