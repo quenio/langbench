d6016467a74ce931d7504344023c907863080d12
quenio <queniodossantos@gmail.com>
2019-03-04 01:23:53 -0300

    Removed the internal module because a language can be either internal or external.

--- lib/tree_lang.rb
+++ lib/tree_lang.rb
@@ -4,137 +4,129 @@ module MPF
 
   module TreeLang
 
-    module Model
+    class Node
 
-      class Node
+      attr_reader :name
+      attr_reader :attributes
+      attr_reader :children
 
-        attr_reader :name
-        attr_reader :attributes
-        attr_reader :children
-
-        def initialize(name, attributes)
-          @name = name
-          @attributes = attributes
-          @children = []
-        end
-
+      def initialize(name, attributes)
+        @name = name
+        @attributes = attributes
+        @children = []
       end
 
     end
 
-    module Internal
+    class Syntax
 
-      class Syntax
-
-        def initialize(options = {})
-          @visitor = options[:visitor]
-        end
-
-        def node(name, attributes = {}, &block)
-          @visitor.visit_node(name, attributes, &block)
-          nil
-        end
-
-        def content(value)
-          @visitor.visit_content(value)
-          nil
-        end
-
-        def evaluate(&source_code)
-          instance_eval &source_code
-        end
-
+      def initialize(options = {})
+        @visitor = options[:visitor]
       end
 
-      class Emitter
+      def node(name, attributes = {}, &block)
+        @visitor.visit_node(name, attributes, &block)
+        nil
+      end
 
-        attr_reader :root
+      def content(value)
+        @visitor.visit_content(value)
+        nil
+      end
 
-        def initialize(root)
-          @root = root
+      def evaluate(&source_code)
+        instance_eval &source_code
+      end
+
+    end
+
+    class Emitter
+
+      attr_reader :root
+
+      def initialize(root)
+        @root = root
+      end
+
+      def emit_code
+        emitter = self
+        TreeLang.source do
+          emitter.emit_node(self, emitter.root)
         end
+      end
 
-        def emit_code
+      def emit_node(syntax, node)
+        if node.is_a? Node
           emitter = self
-          TreeLang.source do
-            emitter.emit_node(self, emitter.root)
+          syntax.node(node.name, node.attributes) do
+            node.children.each { |child| emitter.emit_node(syntax, child) }
+            nil
           end
+        else
+          syntax.content(node)
         end
-
-        def emit_node(syntax, node)
-          if node.is_a? Model::Node
-            emitter = self
-            syntax.node(node.name, node.attributes) do
-              node.children.each { |child| emitter.emit_node(syntax, child) }
-              nil
-            end
-          else
-            syntax.content(node)
-          end
-        end
-
       end
 
-      class Builder
+    end
 
-        include Language::Visitor
+    class Builder
 
-        attr_reader :root
+      include Language::Visitor
 
-        def initialize
-          @parent = []
-        end
+      attr_reader :root
 
-        def enter_node(name, attributes = {}, &block)
-          node = Node.new(name, attributes)
-          @parent.last.children << node unless @parent.empty?
-          @parent.push node
-        end
-
-        def exit_node(name, _attributes = {}, &block)
-          @root = @parent.pop
-        end
-
-        def visit_content(value)
-          @parent.last.children << value
-        end
-
+      def initialize
+        @parent = []
       end
 
-      class Source
+      def enter_node(name, attributes = {}, &block)
+        node = Node.new(name, attributes)
+        @parent.last.children << node unless @parent.empty?
+        @parent.push node
+      end
 
-        def initialize(&source_code)
-          @source_code = source_code
-        end
+      def exit_node(name, _attributes = {}, &block)
+        @root = @parent.pop
+      end
 
-        def print(options)
-          TreeLang.print(options, &@source_code)
-        end
+      def visit_content(value)
+        @parent.last.children << value
+      end
 
-        def render(options)
-          TreeLang.render(options, &@source_code)
-        end
+    end
 
-        def build
-          TreeLang.build(&@source_code)
-        end
+    class Source
 
+      def initialize(&source_code)
+        @source_code = source_code
+      end
+
+      def print(options)
+        TreeLang.print(options, &@source_code)
+      end
+
+      def render(options)
+        TreeLang.render(options, &@source_code)
+      end
+
+      def build
+        TreeLang.build(&@source_code)
       end
 
     end
 
     def self.source(&source_code)
-      Internal::Source.new &source_code
+      Source.new &source_code
     end
 
     def self.emit(options = {})
       model = options[:from]
-      emitter = Internal::Emitter.new(model)
+      emitter = Emitter.new(model)
       emitter.emit_code
     end
 
     def self.build(&source_code)
-      builder = Internal::Builder.new
+      builder = Builder.new
       visit visitor: builder, &source_code
       builder.root
     end
@@ -142,7 +134,7 @@ module MPF
     @syntax = { xml: XML::External::Syntax }
 
     def self.parse(options = {})
-      # builder = Internal::Builder.new
+      # builder = Builder.new
       language = options[:from]
       syntax = @syntax[language].new
       errors = syntax.parse(text: options[:text], visitor: Language::External::ParseTree::Printer.new)
@@ -167,7 +159,7 @@ module MPF
     end
 
     def self.visit(options = {}, &source_code)
-      Internal::Syntax.new(visitor: options[:visitor]).evaluate &source_code
+      Syntax.new(visitor: options[:visitor]).evaluate &source_code
     end
 
   end
